b8d061ce99d9ae8668f957bb0d1d183243b7ea0c
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-04-03 08:55:27 +0900

    Tweetモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_28_231541) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_02_233940) do
+  create_table "tweets", force: :cascade do |t|
+    t.text "content", null: false
+    t.string "image"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -30,4 +39,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_28_231541) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "tweets", "users"
 end
