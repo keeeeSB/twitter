2358b5583790ba1798bda7fd1c2734d27628f887
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-04-04 09:20:13 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   devise_for :users
-  resources :users, only: [ :show ]
+  resources :users, only: [ :show ] do
+    resources :tweets, only: [ :new, :create, :edit, :update, :destroy ]
+  end
+  resources :tweets, only: [ :index ]
 end
