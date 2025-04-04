e33e70cc9953418ab0cb01cdc6f0d3de6cd1758d
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-04-04 09:08:55 +0900

    ツイート機能のテストを追加（TDD）

---
+++ spec/system/tweets_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "ツイート機能", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    sign_in user
+    visit root_path
+  end
+
+  scenario "ユーザーはツイートできる" do
+    click_link "ツイート"
+
+    expect {
+      fill_in "いまどうしてる？", with: "railsなう。"
+    }.to change(user.tweets, :count).by(1)
+
+    expect(page).to have_content "ツイートしました。"
+    expect(current_path).to eq tweets_path
+  end
+end
