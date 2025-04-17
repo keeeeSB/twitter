05fabacd54add2a13978eee148c8fdbff73a0a99
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-04-17 09:15:19 +0900

    Likeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_02_233940) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_17_001404) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "tweet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "tweets", force: :cascade do |t|
     t.text "content", null: false
     t.string "image"
@@ -40,5 +49,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_02_233940) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "tweets"
+  add_foreign_key "likes", "users"
   add_foreign_key "tweets", "users"
 end
