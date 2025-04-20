4dc677918bc01da73e931bac4bc4ceeb05f4882a
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-04-21 08:34:01 +0900

    リレーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   mount_uploader :profile_image, ProfileImageUploader
 
   has_many :tweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_tweet, through: :likes, source: :tweet
 
   validates :name, presence: true
   validates :bio, length: { maximum: 50 }
