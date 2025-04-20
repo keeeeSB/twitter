b21bc85778ebbc9ffaa4a0b16905a3eb05cb9c9d
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-04-21 08:30:04 +0900

    リレーションの設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,8 @@ class Tweet < ApplicationRecord
   mount_uploader :tweet_image, TweetImageUploader
 
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :like_user, through: :likes, source: :user
 
   validates :content, presence: true, length: { maximum: 100 }
 end
