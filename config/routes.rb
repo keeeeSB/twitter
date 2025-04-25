9273cf236b338291fd2c840d19f38ca9b3f8e516
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-04-25 12:12:30 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [ :show ] do
     resources :tweets, only: [ :new, :create, :show, :edit, :update, :destroy ]
+    member do
+      get :followings, :followers
+    end
   end
   resources :tweets, only: [ :index ] do
     resources :likes, only: [ :create, :destroy ]
   end
   resources :likes, only: [ :index ]
+  resources :follows, only: [ :create, :destroy ]
 end
